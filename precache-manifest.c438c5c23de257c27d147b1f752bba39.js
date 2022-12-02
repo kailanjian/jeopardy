b8edc6c40eac64a7941bc9907a05040b69638c73
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2022-12-02 10:34:29 -0800

    Updates

---
+++ precache-manifest.c438c5c23de257c27d147b1f752bba39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a4a4cda13baa99131f62c63a8397a2",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "ac4d2f5d0dc9d0a50005",
+    "url": "/jeopardy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "08cf8724fc4a48e8de05",
+    "url": "/jeopardy/static/css/main.9668de5b.chunk.css"
+  },
+  {
+    "revision": "ac4d2f5d0dc9d0a50005",
+    "url": "/jeopardy/static/js/2.22df44da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jeopardy/static/js/2.22df44da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08cf8724fc4a48e8de05",
+    "url": "/jeopardy/static/js/main.26ca3ad7.chunk.js"
+  },
+  {
+    "revision": "6da609e5bf7e4bbc1480",
+    "url": "/jeopardy/static/js/runtime-main.4404fde2.js"
+  }
+]);
